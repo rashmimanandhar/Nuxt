5cb2f06a83c19057f7bf0afc291ae5bb9fc40ae7
rashmi <rashmi.manandhar7@gmail.com>
2019-05-10 18:22:05 -0400

    listing events from api to listing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
     <h2>Events</h2>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
 export default {
   head() {
     return {
       title: 'Event'
     }
+  },
+  asyncData({ $axios }) {
+    return $axios.get('http://localhost:3000/events').then(response => {
+      return {
+        events: response.data
+      }
+    })
+  },
+  components: {
+    EventCard
   }
 }
 </script>
