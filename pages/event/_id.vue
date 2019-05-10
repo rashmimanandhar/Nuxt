b77b85ca7a349b3771929caf71dedfb6f672f88a
rashmi <rashmi.manandhar7@gmail.com>
2019-05-10 17:32:47 -0400

    get dynamic meta tags and file structuring according to the url

---
+++ pages/event/_id.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>event {{ id }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Event #' + this.id,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'create a event #' + this.id
+        }
+      ]
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.params.id
+    }
+  }
+}
+</script>
